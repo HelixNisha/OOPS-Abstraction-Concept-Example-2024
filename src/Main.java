ab058d8edde952f80ae2be5d7b4d4a0a005ac36b
Admin <nisha.soni@helixtechinc.com>
2024-04-11 12:52:43 -0400

    Abstraction

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Shape circle = new Circle();
+        Shape triangle = new Triangle();
+
+        circle.draw();    // Output: Drawing Circle
+        triangle.draw(); // Output: Drawing Rectangle
+    }
+    }
